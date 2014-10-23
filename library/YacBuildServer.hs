b1334563ae4e3f7e84747cfab053be4e6f44619a
Jan (yac) Matějka <JMatejka@suse.cz>
2014-10-23 18:35:31 +0200

    Run tests on defined machines

--- library/YacBuildServer.hs
+++ library/YacBuildServer.hs
@@ -8,7 +8,8 @@ import Network.Socket
 import Text.Printf
 import Data.Default
 import Data.Ini
-import Data.Text hiding (head, words)
+import Data.Text (pack, unpack)
+import Data.HashMap.Strict (empty, fromList, HashMap, toList, filterWithKey)
 import System.IO.Error
 import System.FilePath.Posix
 import System.Process
@@ -16,17 +17,19 @@ import System.Exit
 import System.IO.Temp
 import System.Directory
 
-data Config = Config
+data ConfigServer = ConfigServer
     { listen_addr   :: Maybe String
     , listen_port   :: Maybe String
     , work_dir      :: Maybe FilePath
+    , machines      :: HashMap Machine Hostname
     }
 
-instance Default Config where
-    def = Config
+instance Default ConfigServer where
+    def = ConfigServer
         { listen_addr = Nothing
         , listen_port = Nothing
         , work_dir    = Nothing
+        , machines    = empty
         }
 
 maybeRight :: Either a b -> Maybe b
@@ -37,16 +40,24 @@ rightOrErr :: Either String Ini -> Ini
 rightOrErr (Right x) = x
 rightOrErr (Left  x) = error x
 
-readConfig :: Maybe FilePath -> IO Config
+readMachinesConfig :: Maybe FilePath -> IO (HashMap Machine Hostname)
+readMachinesConfig Nothing = readMachinesConfig $ Just "/etc/ybs/machines"
+readMachinesConfig (Just fp) = do
+    f <- readFile fp
+    return . fromList . fmap ((\(x:(y:_)) -> (x, y)) . words) $ lines f
+
+readConfig :: Maybe FilePath -> IO ConfigServer
 readConfig Nothing   = readConfig $ Just "/etc/ybs.ini"
 readConfig (Just fp) = do
     ini <- readIniFile fp
     t   <- getTemporaryDirectory
+    ms <- readMachinesConfig Nothing
 
     return $ def
         { listen_addr = gi ini "server" "listen_addr"
         , listen_port = gi ini "server" "listen_port"
         , work_dir    = Just $ combine t "ybs"
+        , machines    = ms
         }
   where
     gi :: Either String Ini -> String -> String -> Maybe String
@@ -71,7 +82,7 @@ ybs_main = do
     _ <- listen sock 5
     loop cg sock
 
-loop :: Config -> Socket -> IO ()
+loop :: ConfigServer -> Socket -> IO ()
 loop cg sock = do
     (conn, addr) <- accept sock
     putStrLn $ printf "connected: %s" (show addr)
@@ -106,12 +117,38 @@ withMkTempWorkDir (Just wdir) tpl m = do
 git_clone :: FilePath -> String -> IO ()
 git_clone wdir uri = do
     printf "git clone %s into %s" uri wdir
-    worker_cmd (Just wdir) "git" ["clone", uri]
+    worker_cmd (Just wdir) "git" ["clone", uri, "repo"]
 
-handle :: Config -> Socket -> IO ()
+handle :: ConfigServer -> Socket -> IO ()
 handle cg conn = do
     (bytes, _, _) <- recvFrom conn 1024
     let words' = words bytes
+        gituri = head words'
+        githead = words' !! 1
 
-    withMkTempWorkDir (work_dir cg) "XXXXXXX" $ \wdir -> do
-        git_clone wdir $ head words'
+    withMkTempWorkDir (work_dir cg) "XXXXXXX." $ \wdir -> do
+        git_clone wdir gituri
+        callCommand $ printf
+            "cd %s && git checkout %s" (wdir </> "repo") githead
+
+        repo_cg <- readFile $ wdir </> "repo" </> ".ybs"
+        distribute cg gituri githead $ lines repo_cg
+
+distribute :: ConfigServer -> String -> String -> [Machine] -> IO ()
+distribute cg gituri githead ms = do
+    (mapM_ (distribute' gituri githead) . toList) . filterWithKey (\k _ -> elem k ms) $ machines cg
+
+distribute' :: String -> String -> (Machine, Hostname) -> IO ()
+distribute' gu gh (m, h) = do
+    putStrLn $ printf "running acceptance testsuite at %s for %s" h m
+    callCommand $ printf
+        "ssh %s git clone %s /tmp/foo" h gu
+
+    callCommand $ printf
+        "ssh %s cd /tmp/foo && git checkout %s" h gh
+
+    callCommand $ printf
+        "ssh %s cd /tmp/foo && cabal test" h
+
+type Hostname = String
+type Machine  = String
